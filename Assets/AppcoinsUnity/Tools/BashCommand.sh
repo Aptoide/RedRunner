2ff8321b54aa6db487f6745b62ca167260a00fa0
Nuno Monteiro <nuno.monteir@gmail.com>
2018-09-10 10:21:20 +0100

    Integrated latest package
    Removed old dll versions

--- Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 osascript -e 'activate application "/Applications/Utilities/Terminal.app"'
-cd '/Users/nunomonteiro/Documents/GitHub/RedRunner_BDS/and/Red Runner'
+cd '/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/and/Red Runner'
 if [ "$('/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
 then
-'/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.appcoins.redrunner/.UnityPlayerActivity' 2>&1 2>'/Users/nunomonteiro/Documents/GitHub/RedRunner_BDS/Assets/AppcoinsUnity/Tools/ProcessError.out'
+'/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.appcoins.sample.UnityPlayerActivity' 2>&1 2>'/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/Assets/AppcoinsUnity/Tools/ProcessError.out'
 else
-echo error: no usb device found > '/Users/nunomonteiro/Documents/GitHub/RedRunner_BDS/Assets/AppcoinsUnity/Tools/ProcessError.out'
+echo error: no usb device found > '/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/Assets/AppcoinsUnity/Tools/ProcessError.out'
 fi
-echo 'done' > '/Users/nunomonteiro/Documents/GitHub/RedRunner_BDS/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
+echo 'done' > '/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
 exit
