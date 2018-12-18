8c1d1ccfb098b5aa202bad18cd72e370a8a79ff2
Bruno Varela <bruno.varela@aptoide.com>
2018-12-18 09:47:38 +0000

    APCC-755-Update-Redrunner
    Installed the latest BDS plugin version: 1.0.3

--- Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 osascript -e 'activate application "/Applications/Utilities/Terminal.app"'
-cd '/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/android/Red Runner'
-if [ "$('/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
+cd '/Users/brunovarela/Documents/sprint_20_11_4_12/Red Runner'
+if [ "$('/Users/brunovarela/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
 then
-'/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.appcoins.redrunner/.UnityPlayerActivity' 2>&1 2>'/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/Assets/AppcoinsUnity/Tools/ProcessError.out'
+'/Users/brunovarela/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.appcoins.redrunner/.UnityPlayerActivity' 2>&1 2>'/Users/brunovarela/Documents/GitHub/RedRunner/Assets/AppcoinsUnity/Tools/ProcessError.out'
 else
-echo error: no usb device found > '/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/Assets/AppcoinsUnity/Tools/ProcessError.out'
+echo error: no usb device found > '/Users/brunovarela/Documents/GitHub/RedRunner/Assets/AppcoinsUnity/Tools/ProcessError.out'
 fi
-echo 'done' > '/Users/nunomonteiro/Documents/GitHub/RedRunnerBDS/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
+echo 'done' > '/Users/brunovarela/Documents/GitHub/RedRunner/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
 exit
